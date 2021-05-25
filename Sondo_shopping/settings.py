19001eb6036a5f2568e4e7b7882348208e128d40
djsbu <zamokuhlemasondo6@gmail.com>
2021-05-25 14:49:06 +0300

    sondos

--- Sondo_shopping/settings.py
+++ Sondo_shopping/settings.py
@@ -135,8 +135,15 @@ CHANNEL_LAYERS = {
 }
 
 
+# DATABASES = {
+#      'default': dj_database_url.config()
+# }
+
 DATABASES = {
-     'default': dj_database_url.config()
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
