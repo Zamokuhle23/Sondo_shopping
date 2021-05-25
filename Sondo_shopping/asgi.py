ed9207d3c5660e62a0f6b484f4a3c1c96d011bf7
djsbu <zamokuhlemasondo6@gmail.com>
2021-05-25 15:05:49 +0300

    sondos

--- Sondo_shopping/asgi.py
+++ Sondo_shopping/asgi.py
@@ -1,13 +1,14 @@
 import os
 
+import django
 from channels.routing import ProtocolTypeRouter,URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 import store.routing
 
-
+# django_asgi_app = get_asgi_application()
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Sondo_shopping.settings')
-
+# django.setup()
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
